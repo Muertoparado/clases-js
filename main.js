664812eff88ad201f5a51e1de7e45b2190cd9a88
Muertoparado <nicolas.casanova20@gmail.com>
2023-04-12 13:35:22 -0500

    feat(clase js): :rocket: pagina

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class lapiz{
     #marca
-    constructor({color="C8FDF00",
+    constructor({color="#FFF700",
         dimension="19",
         marca="Mongol",
         borrador="true",
@@ -13,7 +13,19 @@ class lapiz{
         this.borrador=borrador;
         this.material=material;
     }
- 
-
+    getColor(){
+        return this.color;
+    }
 }
 let obj=undefined;
+let color = document.querySelector(`[name="color"]`);
+let range = document.querySelector(`[name="dimension"]`);
+let marca = document.querySelector(`[name="marca"]`);
+let borrador = document.querySelector(`[name="borrador"]`);
+addEventListener("DOMContentLoaded", (e)=>{
+    range.min = 0;
+    range.max = 100;
+    obj = new lapiz({});
+    color.value = obj.color;
+});
+
