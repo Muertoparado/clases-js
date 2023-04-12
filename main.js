c9243151f5d2f304cf86eac7b57e620ae3f99bd2
Muertoparado <nicolas.casanova20@gmail.com>
2023-04-12 09:30:22 -0500

    feat(clase js):

---
+++ main.js
@@ -0,0 +1,19 @@
+class lapiz{
+    #marca
+    constructor({color="C8FDF00",
+        dimension="19",
+        marca="Mongol",
+        borrador="true",
+        material="Madera"
+
+    }){
+        this.color=color;
+        this.dimension=dimension;
+        this.marca=marca;
+        this.borrador=borrador;
+        this.material=material;
+    }
+ 
+
+}
+let obj=undefined;
