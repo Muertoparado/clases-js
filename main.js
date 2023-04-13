a4ebef673d4df3e22fae96fa77210a589ce28d0a
Muertoparado <nicolas.casanova20@gmail.com>
2023-04-13 09:29:15 -0500

    feat(clase js): :rocket: info nueva

--- main.js
+++ main.js
@@ -13,19 +13,39 @@ class lapiz{
         this.borrador=borrador;
         this.material=material;
     }
-    getColor(){
+  /*   getColor(){
         return this.color;
     }
+    getDimension(){
+        return this.dimension;
+    }
+    getMarca(){
+        return this.marca;
+    } */
 }
 let obj=undefined;
 let color = document.querySelector(`[name="color"]`);
 let range = document.querySelector(`[name="dimension"]`);
 let marca = document.querySelector(`[name="marca"]`);
 let borrador = document.querySelector(`[name="borrador"]`);
+let material = document.querySelector(`[name="material"]`);
+
+/* let dato=obj.getData(); */
+range.value=true
+    marca.checked=true
+    borrador.checked=true;
+    material.checked=true;
+
 addEventListener("DOMContentLoaded", (e)=>{
     range.min = 0;
-    range.max = 100;
-    obj = new lapiz({});
+    range.max = 30;
+    obj = new lapiz({})
     color.value = obj.color;
+    marca.value = obj.marca;
+    material.value = obj.material;
+
+    document.querySelector("#tabla").innerHTML
+   
 });
+
 
